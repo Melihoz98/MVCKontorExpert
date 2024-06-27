403ec00467bdbb2693f0d0ee951212ddb6f6f365
Ahmad <10478728@ucn.dk>
2024-06-27 07:41:07 +0200

    getUsetProducts and getNewProducts implemented

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,22 +10,28 @@ namespace MVCKontorExpert.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ICategoryData _categoryData;
-        private readonly IParentCategoryData _parentCategoryData; // Assuming you have IParentCategoryData
+        private readonly IParentCategoryData _parentCategoryData; 
+        private readonly IProductData _productData;
 
-        public HomeController(ILogger<HomeController> logger, ICategoryData categoryData, IParentCategoryData parentCategoryData)
+        public HomeController(ILogger<HomeController> logger, ICategoryData categoryData, IParentCategoryData parentCategoryData, IProductData productData)
         {
             _logger = logger;
             _categoryData = categoryData;
             _parentCategoryData = parentCategoryData;
+            _productData = productData;
         }
 
         public async Task<IActionResult> Index()
         {
             var categories = await _categoryData.GetAllCategories();
             var parentCategories = await _parentCategoryData.GetAllParentCategories();
+            var usedProducts = await _productData.GetUsedProducts();
+            var newProducts = await _productData.GetNewProducts(); 
 
             ViewBag.Categories = categories;
             ViewBag.ParentCategories = parentCategories;
+            ViewBag.UsedProducts = usedProducts;
+            ViewBag.NewProducts = newProducts;
 
             return View();
         }
